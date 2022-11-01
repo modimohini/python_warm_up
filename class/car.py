6d85f0406bd3b3afb370d894ac36b55cdaa0590e
Mohini Modi <mmmodimohini@gmail.com>
2022-10-31 21:20:10 -0500

    classes

--- class/car.py
+++ class/car.py
@@ -3,11 +3,16 @@ class Car:
         self.make = make
         self.model = model
         self.year = year
+        self.odometer_reding = 0
 
 
     def get_descriptive_name(self):
-        long_name = f"{self.year} {self.model} {self.make}"
+        long_name = f"{self.year} {self.model} {self.make} {self.odometer_reding}"
         return long_name.title()
 
 my_new_car = Car('porsche', '718 Boxter', '2023')
 print(my_new_car.get_descriptive_name())
+
+my_new_car.odometer_reding = 89
+use_car = my_new_car.get_descriptive_name()
+print(use_car)
